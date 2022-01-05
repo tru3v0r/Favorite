1ab187e863eb619026c2e68dfe34d04c4331e395
Duvigneau Charles <charles.duvigneau@gmail.com>
2022-01-05 09:45:37 +0100

    Initial commit

---
+++ src/main/java/fr/eql/al35/service/StockService.java
@@ -0,0 +1,27 @@
+package fr.eql.al35.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.eql.al35.entity.Stock;
+import fr.eql.al35.iservice.StockIService;
+import fr.eql.al35.repository.StockIRepository;
+
+@Service
+public class StockService implements StockIService {
+
+	@Autowired
+	private StockIRepository stockRepository;
+	
+	@Override
+	public Stock displayStockById(int id) {
+		return stockRepository.findById(id).get();
+	}
+	
+	@Override
+	public Stock upDate(Integer id, Stock stock) {
+		stock.setId(id);
+        
+		return stockRepository.save(stock);
+	}
+}
